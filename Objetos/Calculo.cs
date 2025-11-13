5f3c8b6446b14bb9eba1cfd357524abb37c376f4
Juan <jgutierrezluman@gmail.com>
2025-11-13 20:37:25 -0300

    clases datos

---
+++ Objetos/Calculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio2025.Objetos
+{
+    public class Calculo
+    {
+
+        public int Año { get; set; }
+        public int Mes { get; set; }
+        public double TotalVentas { get; set; }
+        public double TotalCompras { get; set; }
+        public double IVAVentas { get; set; }
+        public double IVACompras { get; set; }
+        
+        public double IRAE { get; set; }
+        public DateTime FechaDelCalculo { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Objetos/EnumeradosProyecto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio2025.Objetos
+{
+    public  class EnumeradosProyecto
+    {
+
+        public enum TipoProyecto
+        {
+            PorHora = 1,
+            MontoMensual = 2,
+            PrecioFijo = 3
+        }
+
+        public enum EstadoProyecto
+        {
+            Planificado = 1,
+            EnCurso = 2,
+            Finalizado = 3,
+            Cancelado = 4
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
